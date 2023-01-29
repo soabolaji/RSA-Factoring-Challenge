679e9f24814dfc0aa106713eb9896b00fbef3e4e
soabolaji <soabolaji@gmail.com>
2023-01-28 22:53:37 -0800

    factors

---
+++ fact_func.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * trial_division - Finds the smallest divisor
+ *	if any, of a given number `n`
+ * @n: the number
+ *
+ * Return: smallest divisor if found 0 if n is prime
+*/
+
+int trial_division(long int n)
+{
+	long int f;
+
+	if (n % 2 == 0)
+	{
+		printf("%lu = %lu * %i\n", n, n / 2, 2);
+		return (0);
+	}
+	f = 3;
+
+	while (f * f <= n)
+	{
+	if (n % f == 0)
+	{
+		printf("%lu = %lu * %lu\n", n, n / f, f);
+		return (0);
+	}
+	else
+	{
+		f += 2;
+	}
+
+	}
+
+	printf("%lu = %lu * %i\n", n, n, 1);
+
+	return (0);
+}
